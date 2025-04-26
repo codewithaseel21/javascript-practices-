bdb4e2632df5d5506fa56d329f6baa4166e4255a
muhammadaseel21@gmail.com <muhammadaseel21@gmail.com>
2025-04-26 20:10:17 +0500

    2 chapters completed

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// -------------------chapter 01 Alert---------------------
+
+
+
+// alert ("Thanks for Watching My page")
+
+
+
+// -----------chapter 02 Variables for Strings--------------
+
+
+
+
+// Keyword	Scope Type	Global ban sakta hai?	Wajah
+// var	Function	Haan	Function ke bahar likhne se global ban jata hai
+// let	Block	Nahi	Block ke andar hi limited rehta hai
+// const	Block	Nahi	Block ke andar hi limited rehta hai
+
+
+
+
+
+// var fullName = "Muhammad Aseel Ahmed";
+// console.log(fullName);
+
+// fullName = "Muhammad Aseel Ahmed Qadri"
+// console.log(fullName);
+
+
+// let age = 18
+// console.log(age);
+
+// age = 21
+// console.log(age);
+
+
+// const cast = "gadit"
+// console.log(cast);
+
+// cast = "pardesi"
+// console.log(cast);
+
+
+
